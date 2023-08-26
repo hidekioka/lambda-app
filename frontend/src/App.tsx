78b63a1eebe1b16d8659eb6e86d1a3b01290942b
Henrique Oka <hidekioka@gmail.com>
2023-08-25 21:37:49 -0300

    [019] Small fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,16 @@ import { useState, useEffect } from "react";
 
 import "./App.css";
 
+const lambdaFunction: string = "/web-notes-";
 const webnotesurl: string =
-  "https://nkq7v0s6o2.execute-api.sa-east-1.amazonaws.com/default";
+  "https://zcohs7rk32.execute-api.sa-east-1.amazonaws.com/test-stage" +
+  lambdaFunction;
+const awsRequestHeader = {
+  "X-Amz-Date": "20230825T193850Z",
+  Accept: "*/*",
+  Authorization:
+    "AWS4-HMAC-SHA256 Credential=AKIA5T7VRFX75DFGFHPO/20230825/sa-east-1/execute-api/aws4_request, SignedHeaders=host;x-amz-date, Signature=05fb457da218436df03f463878b770cf372d42b617ea93d0fd2a162653106144",
+};
 
 function App() {
   const [loading, setLoading] = useState(false);
@@ -44,14 +52,17 @@ function App() {
     setLoading(false);
   };
   const loadNotes = async () => {
-    let response: Response = await fetch(webnotesurl + "/web-notes-load");
+    let response: Response = await fetch(webnotesurl + "load", {
+      method: "GET",
+      headers: awsRequestHeader,
+    });
     const data = await response.json();
     setNotes(data.message);
   };
   const deleteNote = async (id: number) => {
     setLoading(true);
     const param = id > 0 ? "?id=" + id : "";
-    await fetch(webnotesurl + "/web-notes-delete" + param);
+    await fetch(webnotesurl + "delete" + param);
     await loadNotes();
     setAlert({ message: "Deleted", show: true, type: "success" });
     setLoading(false);
@@ -59,7 +70,7 @@ function App() {
 
   const createNote = async (text: string) => {
     setLoading(true);
-    await fetch(webnotesurl + "/web-notes-create?text=" + text);
+    await fetch(webnotesurl + "create?text=" + text);
     await loadNotes();
     setAlert({ message: "Created", show: true, type: "success" });
     setLoading(false);
@@ -67,7 +78,7 @@ function App() {
 
   const updateNote = async (id: number, text: string) => {
     setLoading(true);
-    await fetch(webnotesurl + "/web-notes-update?id=" + id + "&text=" + text);
+    await fetch(webnotesurl + "update?id=" + id + "&text=" + text);
     await loadNotes();
     setAlert({ message: "Updated", show: true, type: "success" });
     setLoading(false);
--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 interface Props {
   children: ReactNode;
